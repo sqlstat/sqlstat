a56ae4c911859fe00850538f6b75b036801160ad
iceriver322 <iceriver322@163.com>
2020-08-11 13:12:32 +0800

    path test

--- src/test/java/com/fan/sqlstat/util/FileUtilTest.java
+++ src/test/java/com/fan/sqlstat/util/FileUtilTest.java
@@ -3,11 +3,21 @@ package com.fan.sqlstat.util;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileUtilTest {
 
     @Test
     public void test01(){
         FileUtil.copy(new File("/tmp/1.txt"), new File("/tmp/aaa/bbb/ccc/1.txt"));
+    }
+
+    @Test
+    public void test02() throws IOException {
+        File file = new File("../abc/abc.txt");
+        System.out.println(file.getCanonicalPath());
+        System.out.println(file.getAbsolutePath());
+        System.out.println(file.getName());
+        System.out.println(file.getPath());
     }
 }
