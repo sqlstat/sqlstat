8bd854d6b97025d2caf7d6db76e9e295d0c85447
Jinlongmi <mijl_6011@bupt.edu.cn>
2021-11-07 17:11:24 +0800

    feat: first commit

---
+++ backend/src/main/java/com/gk/sqlstat/SqlstatApplication.java
@@ -0,0 +1,13 @@
+package com.gk.sqlstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlstatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlstatApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/gk/sqlstat/SqlstatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gk.sqlstat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlstatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
